93c406f29562ab71eaa5b14bff5fac02006934ca
jamez <jamez@gccompany.co.kr>
2021-06-16 01:00:13 +0900

    spring transaction 정리 깃 블로그에 추가예정

--- src/main/java/me/hjhng125/springdatajpa/board/BoardRepository.java
+++ src/main/java/me/hjhng125/springdatajpa/board/BoardRepository.java
@@ -3,7 +3,51 @@ package me.hjhng125.springdatajpa.board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 기본적으로 JpaRepository는 트랜잭션이 적용되어 있다.
+ * JpaRepository의 구현체인 SimpleJpaRepository에 이미 클래스레벨에 @Transactional(readOnly = true) 이 적용되어 있음.
+ *
+ * 메서드에 가장 가까운 설정이 우선시된다.
+ *
+ * SimpleJpaRepository를 살펴보면
+ * delete~, save~ 와 같은 메서드들은 @Transactional 이라고 메서드레벨에 정의되어 있으며,
+ * find~와 같은 조회 메서드에는 정의되어 있지 않다.
+ * 이런 경우 @Transactional(readOnly = true)가 적용된다.
+ *
+ * Transactional이 정의되어 있는 메서드 내부에 다른 트랜잭션이 적용된 메서드 여러개가 들어있다면
+ * 가장 상위 메서드의 트랜잭션이 적용된다.
+ *
+ * 이 어노테이션이 적용되어 있는 메서드에서 RuntimeException 이나 Error가 발생하면 Rollback을 시킨다.
+ * CheckedException는 롤백을 안함.
+ *
+ * CheckedException이 발생했을때에 롤백하고 싶다면 rollbackFor, rollbackForClassName 옵션을 주어야하며,
+ * RuntimeException이나 Error에서 롤백하고 싶지 않다면 noRollbackFor, noRollbackForClassName 옵션을 주어야 함.
+ *
+ * timeout을 설정할 수도 있으며, transactionManager를 설정할 수도 있다.
+ * transactionManager는 기본적으로 jpaTransactionManager이다.
+ * transactionManager를 여러개 설정하지 않는 이상 설정할 일은 없을 것이다.
+ * spring boot를 쓰며 transactionManager가 하나일 경우 스프링 자동 설정에서 설정한 transactionManager Bean name이 기본값으로 설정된다.
+ *
+ * readOnly는 해당 트랜잭션이 읽기전용인지 아닌 나타내는 옵션이다.
+ * 이 값을 통해 성능 최적화를 해줄 수 있는 여지가 생긴다.
+ * readOnly는 flush모드를 MANUAL로 설정해준다.
+ * FlushModeType.MANUAL은 commit()이 발생해도 명시적으로 session.flush()를 호출하지 않는한 flush가 실행되지 않는다.
+ * 따라서 해당 트랜잭션 내에서는 entity 객체와 snapshot을 비교하는 dirty checking을 하지 않는다.
+ * 데이터를 변경하는 operation이 없다면 readOnly를 true로 주는 것이 좋다.
+ *
+ * isolation : 여러개의 트랜잭션이 동시에 DB에 접근했을 때 이 트랜잭션들을 어떻게 처리할 것인가
+ *             제한적이게 될 수록 성능은 안좋아진다.
+ *         default : 데이터베이스의 기본값을 따른다.
+ *
+ * propagation : 트랜잭션을 어떻게 전파할 것인가.
+ *               nested 트랜잭션에 대한 트랜잭션 전파옵션.
+ *         default : REQUIRED
+ *
+ * isolation과 propagation은 blog에서 더 깊게 작성할 예정이다.
+ */
+//@Transactional
 public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor<Board>, QueryByExampleExecutor<Board> {
 
 
--- src/test/java/me/hjhng125/springdatajpa/study/StudyRepositoryTest.java
+++ src/test/java/me/hjhng125/springdatajpa/study/StudyRepositoryTest.java
@@ -86,8 +86,10 @@ class StudyRepositoryTest {
          * 위에서 업데이트를 했고 쿼리도 정상적으로 발생했지만 아래의 테스트는 실패한다.
          * 왜냐하면 아직 트랜잭션이 끝나지 않았기에 select query가 발생하지 않고,
          * 캐시에 남아있는 'spring data jpa'라는 이름의 객체를 그대로 리턴했기 때문이다.
+         *
          * 위의 업데이트는 사실상 persistenceContext에서 관리하는 객체를 수정하지 않고 DB의 데이터를 바로 수정했다.
          * 따라서 DB에 쿼리를 날려야만 반영된 것을 알 수 있기에 지금 이 트랜잭션에서는 변경되었음을 알 수가 없다.
+         *
          * 이 방법을 해결하기 위해서 repository에 명시한 @Modifying에 clearAutomatically = true를 주는 것이 하나의 방법이 될 것이다.
          * 쿼리 실행 후 persistenceContext를 clear해주면 select쿼리가 왔을 때 persistenceContext가 비워져있으니 새로 DB에서 읽어온다.
          * */
